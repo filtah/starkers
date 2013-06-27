3e5c145fa3df8485841b3a2604266484e67cb367
filtah <djphilsea@hotmail.com>
2013-06-27 23:41:11 +0200

    Update functions.php
    I'm not an expert in html5 markup, but shouldn't the pubdate attribute be on the <time> tag, not the <a> tag?

--- functions.php
+++ functions.php
@@ -90,7 +90,7 @@
 			<article id="comment-<?php comment_ID() ?>">
 				<?php echo get_avatar( $comment ); ?>
 				<h4><?php comment_author_link() ?></h4>
-				<time><a href="#comment-<?php comment_ID() ?>" pubdate><?php comment_date() ?> at <?php comment_time() ?></a></time>
+				<time pubdate><a href="#comment-<?php comment_ID() ?>"><?php comment_date() ?> at <?php comment_time() ?></a></time>
 				<?php comment_text() ?>
 			</article>
 		<?php endif;
